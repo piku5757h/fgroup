3b68cf2a1340c768394796b0288644737b148b55
codewithsatyajit <satyajitkk3032@gmail.com>
2024-12-28 08:30:02 +0530

    crew

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import HomePage from "./page/HomePage"
+
+
+const App = () => {
+  return (
+    <>
+       <HomePage/>
+           {/* <Route path="/" element={<HomePage />} /> */}
+    
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/page/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from "../Layout/Navbar"
+import Banner from "../components/Banner.jsx"
+import Footer from "../Layout/Footers.jsx"
+
+
+const HomePage = () => {
+  return (
+    <>
+      <Navbar />
+      <Banner />
+      <Footer/>
+    </>  
+  )
+}
+
+export default HomePage
